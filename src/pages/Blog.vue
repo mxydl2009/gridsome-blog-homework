5fce1e666909837336de81c0fb07b40290ce33d6
mxydl2009 <mxydl2009@163.com>
2020-12-05 22:24:11 +0800

    feat: 完成Blog.vue和Project.vue的模板

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,6 +1,205 @@
 <template>
   <Layout>
-    
+    <div class="el-col el-col-18" style="padding-left: 10px;">
+      <section>
+        <div style="min-height: 600px;" class="">
+          <div class="el-card is-never-shadow" style="margin-bottom: 20px;"><!---->
+            <div class="el-card__body">
+              <div class="el-input" style="width: 300px;"><!---->
+                <input type="text" autocomplete="off" placeholder="请输入关键字" class="el-input__inner"><!----><!----><!---->
+              </div>
+              <button type="button" class="el-button el-button--default is-plain is-circle" style="margin-left: 10px;"><!---->
+                <i class="el-icon-search"></i><!---->
+              </button>
+              <button type="button" class="el-button el-button--warning is-plain is-circle" style="margin-left: 10px;"><!---->
+                <i class="el-icon-share"></i><!---->
+              </button>
+              <button type="button" class="el-button el-button--primary is-plain is-round" style="float: right;"><!---->
+                <i class="el-icon-edit"></i>
+                <span>写博文</span>
+              </button>
+            </div>
+          </div>
+          <div>
+            <div class="el-card is-hover-shadow" style="margin-bottom: 20px;">
+              <div class="el-card__header">
+                <div>
+                  <div class="el-row">
+                    <div class="el-col el-col-16">
+                      <span>
+                        <a style="text-decoration: none; cursor: pointer;">
+                          <i class="el-icon-edit-outline"></i>&nbsp;&nbsp; 2018.5.19 更新
+                        </a>
+                      </span>
+                    </div>
+                    <div class="el-col el-col-8">
+                      <div style="text-align: right;">
+                        <button type="button" class="el-button el-button--text" style="padding: 3px 0px;"><!---->
+                          <i class="el-icon-share"></i><!---->
+                        </button> <!----> <!---->
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="el-card__body">
+                <div style="font-size: 0.9rem; line-height: 1.5; color: rgb(96, 108, 113);">
+                最近更新 2018-05-19 01:10:06
+                </div>
+                <div style="font-size: 1.1rem; line-height: 1.5; color: rgb(48, 49, 51); padding: 10px 0px 0px;">
+                  1 增加使用帮助
+                  2 改进readme不存在时的情况
+                </div>
+              </div>
+            </div>
+            <div class="el-card is-hover-shadow" style="margin-bottom: 20px;">
+              <div class="el-card__header">
+                <div>
+                  <div class="el-row">
+                    <div class="el-col el-col-16">
+                      <span>
+                        <a style="text-decoration: none; cursor: pointer;">
+                          <i class="el-icon-edit-outline"></i>&nbsp;&nbsp; 2018.5.15 更新
+                        </a>
+                      </span>
+                    </div>
+                    <div class="el-col el-col-8">
+                      <div style="text-align: right;">
+                        <button type="button" class="el-button el-button--text" style="padding: 3px 0px;"><!---->
+                          <i class="el-icon-share"></i><!---->
+                        </button> <!----> <!---->
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="el-card__body">
+              <div style="font-size: 0.9rem; line-height: 1.5; color: rgb(96, 108, 113);">
+                最近更新 2018-05-15 21:40:04
+              </div> 
+              <div style="font-size: 1.1rem; line-height: 1.5; color: rgb(48, 49, 51); padding: 10px 0px 0px;">
+                1 优化粉丝页面
+                2 增加是否自动播放音乐配置
+              </div>
+              </div>
+            </div>
+            <div class="el-card is-hover-shadow" style="margin-bottom: 20px;">
+              <div class="el-card__header">
+                <div>
+                  <div class="el-row">
+                    <div class="el-col el-col-16">
+                      <span>
+                        <a style="text-decoration: none; cursor: pointer;">
+                          <i class="el-icon-edit-outline"></i>&nbsp;&nbsp; 2018.5.13 更新
+                        </a>
+                      </span>
+                    </div>
+                    <div class="el-col el-col-8">
+                      <div style="text-align: right;">
+                        <button type="button" class="el-button el-button--text" style="padding: 3px 0px;"><!---->
+                          <i class="el-icon-share"></i><!---->
+                        </button> <!----> <!---->
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="el-card__body">
+                <div style="font-size: 0.9rem; line-height: 1.5; color: rgb(96, 108, 113);">
+                  最近更新 2018-05-13 16:49:11
+                </div>
+                <div style="font-size: 1.1rem; line-height: 1.5; color: rgb(48, 49, 51); padding: 10px 0px 0px;">
+                  增加了社交圈, 查看粉丝以及关注用户列表
+                </div>
+              </div>
+            </div>
+            <div class="el-card is-hover-shadow" style="margin-bottom: 20px;">
+              <div class="el-card__header">
+                <div>
+                  <div class="el-row">
+                    <div class="el-col el-col-16">
+                      <span>
+                        <a style="text-decoration: none; cursor: pointer;">
+                          <i class="el-icon-edit-outline"></i>&nbsp;&nbsp; VBlog项目结构
+                        </a>
+                      </span>
+                    </div>
+                    <div class="el-col el-col-8">
+                      <div style="text-align: right;">
+                        <button type="button" class="el-button el-button--text" style="padding: 3px 0px;"><!---->
+                          <i class="el-icon-share"></i><!---->
+                        </button> <!----> <!---->
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="el-card__body">
+                <div style="font-size: 0.9rem; line-height: 1.5; color: rgb(96, 108, 113);">
+                  最近更新 2018-05-12 14:56:51
+                </div>
+                <div style="font-size: 1.1rem; line-height: 1.5; color: rgb(48, 49, 51); padding: 10px 0px 0px;">
+                  介绍一下这个项目的代码结构, 不定时更新文档
+                </div>
+              </div>
+            </div>
+            <div class="el-card is-hover-shadow" style="margin-bottom: 20px;">
+              <div class="el-card__header">
+                <div>
+                  <div class="el-row">
+                  <div class="el-col el-col-16">
+                    <span>
+                      <a style="text-decoration: none; cursor: pointer;">
+                        <i class="el-icon-edit-outline"></i>&nbsp;&nbsp; 格式化了所有代码的缩进
+                      </a>
+                    </span>
+                  </div>
+                  <div class="el-col el-col-8">
+                    <div style="text-align: right;">
+                      <button type="button" class="el-button el-button--text" style="padding: 3px 0px;"><!---->
+                        <i class="el-icon-share"></i><!---->
+                      </button> <!----> <!---->
+                    </div>
+                  </div>
+                  </div>
+                </div>
+              </div>
+              <div class="el-card__body">
+                <div style="font-size: 0.9rem; line-height: 1.5; color: rgb(96, 108, 113);">
+                  最近更新 2018-05-11 15:10:06
+                </div>
+                <div style="font-size: 1.1rem; line-height: 1.5; color: rgb(48, 49, 51); padding: 10px 0px 0px;">
+                  把所有代码的缩进从制表符统一为4个空格
+                </div>
+              </div>
+            </div>
+            <div style="text-align: center;">
+              <div class="el-pagination is-background">
+                <button type="button" disabled="disabled" class="btn-prev">
+                  <i class="el-icon el-icon-arrow-left"></i>
+                </button>
+                <ul class="el-pager">
+                  <li class="number active">1</li><!---->
+                  <li class="number">2</li>
+                  <li class="number">3</li><!---->
+                  <li class="number">4</li>
+                </ul>
+                <button type="button" class="btn-next">
+                  <i class="el-icon el-icon-arrow-right"></i>
+                </button>
+              </div>
+            </div>
+          </div> <!---->
+          <div class="el-loading-mask" style="display: none;">
+          <div class="el-loading-spinner">
+            <svg viewBox="25 25 50 50" class="circular">
+              <circle cx="50" cy="50" r="20" fill="none" class="path"></circle>
+            </svg><!---->
+          </div>
+          </div>
+        </div>
+      </section>
+    </div>
   </Layout>
 </template>
 
--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,6 +1,101 @@
 <template>
   <Layout>
-    
+    <div class="el-col el-col-18" style="padding-left: 10px;">
+      <section>
+        <div style="min-height: 600px;" class="">
+          <div class="el-card is-never-shadow" style="margin-bottom: 20px;"><!---->
+            <div class="el-card__body">
+              <div class="el-input" style="width: 300px;"><!---->
+                <input type="text" autocomplete="off" placeholder="请输入关键字" class="el-input__inner"><!----><!----><!---->
+              </div>
+              <button type="button" class="el-button el-button--default is-plain is-circle" style="margin-left: 10px;"><!---->
+                <i class="el-icon-search"></i><!---->
+              </button>
+              <button type="button" class="el-button el-button--warning is-plain is-circle" style="margin-left: 10px;"><!---->
+                <i class="el-icon-share"></i><!---->
+              </button>
+            </div>
+          </div>
+          <div>
+            <div class="el-card is-hover-shadow" style="margin-bottom: 20px;">
+              <div class="el-card__header">
+                <div>
+                  <div class="el-row">
+                    <div class="el-col el-col-16">
+                      <span>
+                        <a style="text-decoration: none; cursor: pointer;">
+                          <i class="el-icon-service"></i>&nbsp;&nbsp; canvas-utils
+                        </a>
+                      </span>
+                    </div>
+                  <div class="el-col el-col-8">
+                    <div style="text-align: right;">
+                      <button type="button" class="el-button el-button--text" style="padding: 3px 0px;"><!---->
+                        <i class="el-icon-back"></i>
+                        <span>前往GitHub</span>
+                      </button>
+                      <button type="button" class="el-button el-button--text" style="padding: 3px 0px;"><!---->
+                        <i class="el-icon-share"></i><!---->
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="el-card__body">
+              <div style="font-size: 0.9rem; line-height: 1.5; color: rgb(96, 108, 113);">
+                最近更新 2020-01-21 22:23:50
+              </div>
+              <div style="font-size: 1.1rem; line-height: 1.5; color: rgb(48, 49, 51); padding: 10px 0px 0px;">
+                Canvas utils
+              </div>
+              <div style="font-size: 1.1rem; color: rgb(48, 49, 51); padding: 10px 0px 0px;">
+                <div class="el-row">
+                  <div class="el-col el-col-16" style="padding-top: 5px;">
+                    <i class="el-icon-star-off el-tooltip" aria-describedby="el-tooltip-8783" tabindex="0" style="margin: 0px 5px 0px 0px;"></i>
+                      0
+                    <i class="el-icon-view el-tooltip" aria-describedby="el-tooltip-4658" tabindex="0" style="margin: 0px 5px 0px 15px;"></i>
+                      0
+                    <i class="el-icon-bell el-tooltip" aria-describedby="el-tooltip-599" tabindex="0" style="margin: 0px 5px 0px 15px;"></i>
+                      0
+                  </div>
+                  <div class="el-col el-col-8" style="text-align: right;">
+                    <span class="el-tag el-tag--danger el-tag--small">MIT<!----></span>
+                    <span class="el-tag el-tag--success el-tag--small">JavaScript<!----></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>  
+            <div style="text-align: center;">
+              <div class="el-pagination is-background">
+                <button type="button" disabled="disabled" class="btn-prev">
+                  <i class="el-icon el-icon-arrow-left"></i>
+                </button>
+                <ul class="el-pager">
+                  <li class="number active">1</li><!---->
+                  <li class="number">2</li>
+                  <li class="number">3</li>
+                  <li class="number">4</li>
+                  <li class="number">5</li><!---->
+                  <li class="number">6</li>
+                </ul>
+                <button type="button" class="btn-next">
+                  <i class="el-icon el-icon-arrow-right"></i>
+                </button>
+              </div>
+            </div>
+          </div> <!---->
+          <div class="el-loading-mask" style="display: none;">
+          <div class="el-loading-spinner">
+            <svg viewBox="25 25 50 50" class="circular">
+              <circle cx="50" cy="50" r="20" fill="none" class="path"></circle>
+            </svg><!---->
+          </div>
+          </div>
+        </div>
+      </section>
+    </div>
   </Layout>
 </template>
 
