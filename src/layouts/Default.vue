c2157a725cc14c120555cc309455705ad408bfbc
mxydl2009 <mxydl2009@163.com>
2020-12-05 18:41:48 +0800

    feat:完成页面模板Default.vue

---
+++ src/layouts/Default.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="layout">
+    <header class="header">
+      <link rel="stylesheet" href="/css/index.css">
+      <section class="page-header" style="background-image: linear-gradient(120deg, rgb(38, 144, 249), rgb(252, 45, 45)); color: rgb(255, 255, 255);">
+        <div style="position: absolute; top: 20px; right: 20px; z-index: 2;">
+          <button type="button" class="el-button el-tooltip el-button--default is-circle" aria-describedby="el-tooltip-268" tabindex="0">
+            <!----><i class="el-icon-rank"></i>
+            <!---->
+          </button>
+        </div>
+        <div style="position: absolute; top: 114px; left: 597px; z-index: 1;">
+          <font style="font-size: 22px; color: rgb(255, 255, 255);">
+            <b>♪</b>
+          </font>
+        </div>
+        <div style="position: absolute; top: 141px; left: 76px; z-index: 1;">
+          <font style="font-size: 26px; color: rgb(255, 255, 255);">
+            <b>♪</b>
+          </font>
+        </div>
+        <div style="position: absolute; top: 78px; left: 103px; z-index: 1;">
+          <font style="font-size: 34px; color: rgb(255, 255, 255);">
+            <b>♪</b>
+          </font>
+        </div>
+        <div style="position: absolute; top: 172px; left: 599px; z-index: 1;">
+          <font style="font-size: 39px; color: rgb(255, 255, 255);">
+            <b>♪</b>
+          </font>
+        </div>
+        <div style="position: absolute; top: 197px; left: 106px; z-index: 1;">
+          <font style="font-size: 35px; color: rgb(255, 255, 255);"><b>♪</b></font>
+        </div>
+        <div style="position: absolute; top: 27px; left: 497px; z-index: 1;">
+          <font style="font-size: 39px; color: rgb(255, 255, 255);"><b>♪</b></font>
+        </div>
+        <div style="position: absolute; top: 145px; left: 465px; z-index: 1;">
+          <font style="font-size: 24px; color: rgb(255, 255, 255);"><b>♪</b></font>
+        </div>
+        <div style="position: absolute; top: 48px; left: 495px; z-index: 1;">
+          <font style="font-size: 33px; color: rgb(255, 255, 255);"><b>♪</b></font>
+        </div>
+        <div style="position: absolute; top: 250px; left: 503px; z-index: 1;">
+          <font style="font-size: 21px; color: rgb(255, 255, 255);"><b>♪</b></font>
+        </div>
+        <div style="position: absolute; top: 185px; left: 16px; z-index: 1;">
+          <font style="font-size: 20px; color: rgb(255, 255, 255);"><b>♪</b></font>
+        </div>
+        <div style="position: absolute; top: 99px; left: 587px; z-index: 1;">
+          <font style="font-size: 38px; color: rgb(255, 255, 255);"><b>♪</b></font>
+        </div>
+        <div style="position: absolute; top: 139px; left: 487px; z-index: 1;">
+          <font style="font-size: 20px; color: rgb(255, 255, 255);"><b>♪</b></font>
+        </div>
+        <h1 class="project-name">Laziji</h1>
+        <h2 class="project-tagline">欢迎来到辣子鸡的个人博客。</h2>
+        <a href="https://github.com/GitHub-Laziji" target="_blank" class="btn">GitHub主页</a>
+        <a href="https://github.com/GitHub-Laziji/vblog" target="_blank" class="btn">博客源码</a>
+      </section>
+      <div style="position: relative; z-index: 2; margin: -30px auto auto; width: 64rem;">
+        <div class="el-card is-never-shadow">
+          <!---->
+          <div class="el-card__body" style="padding: 0px;">
+            <div class="el-row">
+              <div class="el-col el-col-10">
+                <ul role="menubar" class="el-menu--horizontal el-menu">
+                  <li role="menuitem" aria-haspopup="true" class="el-submenu" tabindex="0">
+                    <div class="el-submenu__title" style="border-bottom-color: transparent;">
+                      了解博主<i class="el-submenu__icon-arrow el-icon-arrow-down"></i>
+                    </div>
+                    <div class="el-menu--horizontal" style="display: none;">
+                      <ul role="menu" class="el-menu el-menu--popup el-menu--popup-bottom-start">
+                        <li role="menuitem" tabindex="-1" class="el-menu-item">github主页</li>
+                        <li role="menuitem" tabindex="-1" class="el-menu-item">其他博客</li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li role="menuitem" aria-haspopup="true" class="el-submenu" tabindex="0">
+                    <div class="el-submenu__title" style="border-bottom-color: transparent;">
+                      其他网站<i class="el-submenu__icon-arrow el-icon-arrow-down"></i>
+                    </div>
+                    <div class="el-menu--horizontal" style="display: none;">
+                      <ul role="menu" class="el-menu el-menu--popup el-menu--popup-bottom-start">
+                        <li role="menuitem" tabindex="-1" class="el-menu-item">segmentfault</li>
+                      </ul>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="el-col el-col-8" style="text-align: center; padding: 12px 0px 0px 10px;">
+                <div class="el-row">
+                  <div class="el-col el-col-4">
+                    <span>
+                      <button type="button" class="el-button el-button--default is-circle el-popover__reference" id="play" aria-describedby="el-popover-7956" tabindex="0">
+                        <!---->
+                        <i class="el-icon-caret-right"></i>
+                        <!---->
+                      </button>
+                    </span>
+                  </div>
+                  <div class="el-col el-col-14" style="padding-left: 20px;">
+                    <div role="slider" aria-valuemin="0" aria-valuemax="0" aria-orientation="horizontal" class="el-slider" aria-valuetext="0" aria-label="slider between 0 and 0" style="width: 100%;">
+                      <!---->
+                      <div class="el-slider__runway">
+                        <div class="el-slider__bar" style="left: 0%;"></div>
+                        <div tabindex="0" class="el-slider__button-wrapper">
+                          <div class="el-slider__button el-tooltip" aria-describedby="el-tooltip-6830" tabindex="0"></div>
+                        </div>
+                        <!---->
+                      </div>
+                    </div>
+                  </div>
+                  <div class="el-col el-col-6" style="padding: 9px 0px 0px 10px; color: rgb(144, 147, 153); font-size: 13px;">
+                    00:00/00:00
+                  </div>
+                </div>
+                <audio loop="loop">
+                  <source src="http://sc1.111ttt.cn:8282/2018/1/03m/13/396131232171.m4a?tflag=1519095601&amp;pin=6cd414115fdb9a950d827487b16b5f97#.mp3" type="audio/mpeg">
+                </audio>
+              </div>
+              <div class="el-col el-col-4" style="text-align: right;">
+                <div style="font-size: 20px; color: rgb(96, 98, 102); margin-top: 5px;">
+                  <b>GitHub-Laziji</b>
+                </div>
+                <div style="color: rgb(96, 98, 102);">
+                  <i class="el-icon-location"></i>&nbsp;fuzhou
+                  <br>
+                </div>
+              </div>
+              <div class="el-col el-col-2" style="text-align: center;">
+                <img class="el-popover__reference" aria-describedby="el-popover-3421" tabindex="0" src="https://avatars0.githubusercontent.com/u/30425217?v=4" style="margin-top: 4px; margin-right: 10px; width: 52px; height: 52px; border-radius: 5px; border: 1px solid rgb(235, 238, 245);">
+                <span>
+                  <div role="tooltip" id="el-popover-3421" aria-hidden="true" class="el-popover el-popper" tabindex="0" style="width: 200px; display: none;">
+                    <div class="el-popover__title">
+                      GitHub-Laziji
+                    </div>
+                    <i class="el-icon-star-on"></i> 辣子鸡
+                    <br>
+                    <i class="el-icon-location"></i> fuzhou
+                    <br>
+                    <img src="https://avatars0.githubusercontent.com/u/30425217?v=4" style="width: 200px; height: 200px;">
+                  </div>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+
+</static-query>
+
+<style>
+
+</style>
